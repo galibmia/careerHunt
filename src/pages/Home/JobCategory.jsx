a9c075ae7869b8f5b84f9e991d964f85abf53eb7
Galib <gm71bd@gmail.com>
2024-10-07 12:54:58 +0600

    Make Job category section responsive

--- src/pages/Home/JobCategory.jsx
+++ src/pages/Home/JobCategory.jsx
@@ -6,11 +6,11 @@ import icon4 from "../../assets/icons/chip 1.svg"
 
 const JobCategory = () => {
     return (
-        <div className='my-32'>
-            <h1 className='text-5xl font-extrabold text-center'>Job Category List</h1>
-            <p className='text-center text-gray-400 my-4'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+        <div className='my-14 md:my-32'>
+            <h1 className='text-2xl md:text-5xl font-extrabold text-center'>Job Category List</h1>
+            <p className='text-center text-gray-400 my-4 px-10 md:px-0'>Explore thousands of job opportunities with all the information you need. Its your future</p>
 
-            <div className='grid grid-cols-4 gap-8 mt-8'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-8 mx-4 md:mx-0'>
                 <div className='bg-[#f9f9ff] rounded-md p-10'>
                     <img className='bg-[#efedff] rounded-lg p-2' src={icon1} alt="" />
                     <h3 className='text-xl font-bold my-4 text-[#474747]'>Account & Finance</h3>
